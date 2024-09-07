a2360de326c778b4b8ac4d87a5ee35a4b5bef3e0
BibekBhusal0 <138126735+BibekBhusal0@users.noreply.github.com>
2024-09-07 18:17:23 +0545

    new start

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-center">
+        <h1 className="text-4xl">Heading </h1>
+        <p className="text-lg">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos
+          in adipisci facere quis dicta nisi asperiores! Molestias cum ullam ut
+          quos sit id ipsam quasi qui, aperiam distinctio, error libero!
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
